47649c5e0994065d7918ba20f614e895fe577781
Rezzubs <marten.roots@gmail.com>
2022-10-05 21:41:24 +0300

    fixed error with removed colors

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -254,7 +254,7 @@ screens = [
                 widget.Spacer(length = 14),
             ],
 40,
-background = bg_dark,
+background = cp["crust"],
 margin = [0, 0, gap, 0],
 # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
 # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
