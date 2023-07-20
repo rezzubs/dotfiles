df1a12e68034fcee9253488cad866d5304a9973d
Marten Roots <marten.roots@gmail.com>
2023-07-20 09:52:16 +0300

    set up formatting for hls

--- private_dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,24 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "hls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer" }
+
+local custom_formatter = function(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = true
+  client.server_capabilities.documentRangeFormattingProvider = false
+
+  local utils = require "core.utils"
+
+  utils.load_mappings("lspconfig", { buffer = bufnr })
+
+  if client.server_capabilities.signatureHelpProvider then
+    require("nvchad_ui.signature").setup(client)
+  end
+
+  if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,5 +30,10 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
+lspconfig.hls.setup {
+  on_attach = custom_formatter,
+  capabilities = capabilities,
+}
+
+--
 -- lspconfig.pyright.setup { blabla}
