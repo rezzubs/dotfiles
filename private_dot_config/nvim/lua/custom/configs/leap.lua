95ac8f2c9c4865a3926d609e56d322936ca6385c
rezzubs <marten.roots@gmail.com>
2023-11-27 10:31:11 +0200

    fresh start

--- private_dot_config/nvim/lua/custom/configs/leap.lua
+++
@@ -1,7 +0,0 @@
-require("leap").add_default_mappings()
-
--- bi-directional search
-vim.keymap.set("n", "s", function()
-  local current_window = vim.fn.win_getid()
-  require("leap").leap { target_windows = { current_window } }
-end)
--- private_dot_config/nvim/lua/custom/init.lua
+++
@@ -1,6 +0,0 @@
--- Font settings for NeoVide
-vim.o.guifont = "Fira Code Medium:h15"
-vim.o.hlsearch = false
-vim.o.relativenumber = true
-vim.o.colorcolumn = "80"
-vim.o.scrolloff = 8
