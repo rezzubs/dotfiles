39a99470bc936f37dd11c013168eaa7501b3e9d5
rezzubs <rezzubs@proton.me>
2024-03-14 10:28:58 +0200

    Add neovide settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,10 @@ require("custom.options")
 require("custom.keymap")
 require("custom.autocommands")
 
+if vim.g.neovide then
+	require("custom.neovide")
+end
+
 -- Bootstrap lazy.nvim
 -- :help lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
