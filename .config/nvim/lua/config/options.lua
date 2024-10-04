db930048fa8e922b73e24714038c31f11abe23cc
rezzubs <rezzubs@proton.me>
2024-10-04 18:45:40 +0300

    Add zig lsp to nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- https://github.com/vim/vim/blob/master/runtime/ftplugin/zig.vim
+vim.g.zig_fmt_autosave = false
