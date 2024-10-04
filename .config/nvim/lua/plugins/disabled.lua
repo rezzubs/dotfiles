18d85e334a6b2fa12607691c274d6d667cfc89d6
rezzubs <rezzubs@proton.me>
2024-10-04 16:01:10 +0300

    Remove unused plugins from nvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -4,4 +4,9 @@ return {
   { "nvim-lualine/lualine.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
