1362f37d44e706e5bf475ff9b5bac6ae1268246a
rezzubs <rezzubs@proton.me>
2024-10-23 10:06:26 +0300

    Debloat nvim

---
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -0,0 +1,55 @@
+return { -- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+			opts = {
+				max_lines = 10,
+			},
+		},
+		{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	},
+
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+					include_surrounding_whitespace = true,
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+					},
+				},
+			},
+		})
+	end,
+}
