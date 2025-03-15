152c3e835dc034ec102d6fa261f61baa011a8987
Marten Roots <rezzubs@proton.me>
2025-03-15 22:21:55 +0200

    zsh: Add yazi

---
+++ .zshrc.d/yazi.sh
@@ -0,0 +1,10 @@
+if command -v yazi 2>&1 > /dev/null; then
+  function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+  }
+fi
