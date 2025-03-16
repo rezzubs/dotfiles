88e267712992d6554d7d7ee158de485c8e0630f4
Marten Roots <rezzubs@proton.me>
2025-03-16 12:55:47 +0200

    wezterm: Update visuals

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local colorscheme = "Catppuccin Mocha"
 local colors = wezterm.color.get_builtin_schemes()[colorscheme]
-local bg = "#131313"
+local bg = "#000000"
 colors.background = bg
 colors.tab_bar.background = bg
 colors.tab_bar.active_tab.bg_color = "white"
@@ -14,7 +14,7 @@ local config = {
 	tab_bar_at_bottom = true,
 	hide_tab_bar_if_only_one_tab = true,
 	colors = colors,
-	font_size = 14,
+	font_size = 13,
 	show_new_tab_button_in_tab_bar = false,
 }
 
