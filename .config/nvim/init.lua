b838b8b9024892bb3927afb3ee803c2754bbb77e
Marten Roots <rezzubs@proton.me>
2025-05-05 11:40:18 +0300

    neovim: Add config base

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("config.opts")
+require("config.lazy")
---
+++ .config/nvim/lua/config/opts.lua
@@ -0,0 +1,21 @@
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+
+o.expandtab = true
+
+o.scrolloff = 10
+o.cursorline = true
+o.colorcolumn = {"+1"}
+o.signcolumn = "yes"
+
+o.undofile = true
+
+o.confirm = true
+
+o.splitright = true
+o.splitbelow = true
+
+o.ignorecase = true
+o.smartcase = true
