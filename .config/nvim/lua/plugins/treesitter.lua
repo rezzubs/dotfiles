e73018b329aaa398d3a7d090e7811a68b733170f
Marten Roots <rezzubs@proton.me>
2025-05-07 18:45:50 +0300

    nvim: Add treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			textobjects = {
+				select = {
+					enable = true,
+
+					lookahead = true,
+
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["as"] = "@local.scope",
+					},
+				},
+				move = {
+					enable = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]a"] = "@parameter.inner",
+					},
+					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[a"] = "@parameter.inner",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+						["[A"] = "@parameter.inner",
+					},
+				},
+			},
+		})
+	end,
+}
